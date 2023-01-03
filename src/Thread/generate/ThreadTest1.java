b481959027aff8e8ce1b3cdb8e09de433b5eac70
seoyeon0203 <w2222@e-mirim.hs.kr>
2023-01-03 14:36:14 +0900

    2022 겨울방학 방과후 Thread 학습

---
+++ src/Thread/generate/ThreadTest1.java
@@ -0,0 +1,24 @@
+package Thread.generate;
+
+public class ThreadTest1 extends Thread {
+
+	@Override
+	public void run() {
+		for(int i=0; i <100; i ++) {
+			System.out.println("thread 실행"+(i+1));
+			try {
+				sleep(500); // 0.5초 간격으로 반복문이 실행 됨.  
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		ThreadTest1 t1 = new ThreadTest1(); // new born 상태
+		t1.start(); // runnable 상태
+
+	}
+
+}
---
+++ src/Thread/generate/ThreadTest2.java
@@ -0,0 +1,27 @@
+package Thread.generate;
+
+public class ThreadTest2 implements Runnable {
+
+	@Override
+	public void run() {
+		for(int i=0; i < 100; i++) {
+			System.out.println("Runnable을 사용한 Thread"+(i+1));
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		ThreadTest2 t2 = new ThreadTest2();
+		Thread t = new Thread(t2); // new born 상태
+		t.start();
+	
+
+	}
+
+}
